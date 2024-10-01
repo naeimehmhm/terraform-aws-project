2b5f11daaf87e53ec2d828cba5fcaeca36b72c19
Naeimeh <naeimeh@Naeimehs-Air.local>
2024-10-01 11:04:52 +0200

    ädd nat gateway

--- Network.tf
+++ Network.tf
@@ -10,20 +10,45 @@ resource "aws_vpc" "wordpress-vpc" {
 
 # # 2. Create Internet Gateway
 
-resource "aws_internet_gateway" "igw-dev" {
+resource "aws_internet_gateway" "wordpress-IGW" {
   vpc_id = aws_vpc.wordpress-vpc.id
   tags = {
-    Name = "IGW-Dev"
+    Name = "wordpress-IGW"
   }
 }
 
+# # 3. Create Custom Route Table
 
+resource "aws_route_table" "wordpress-rtb-public" {
+  vpc_id = aws_vpc.wordpress-vpc.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.wordpress-IGW.id
+  }
+
+  tags = {
+    Name = "wordpress-rtb-public"
+  }
+}
+
+resource "aws_route_table" "wordpress-rtb-private" {
+  vpc_id = aws_vpc.wordpress-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.wordpress-IGW.id
+  }
+
+  tags = {
+    Name = "wordpress-rtb-private"
+  }
+}
 # # 4. Create a Subnet 
 resource "aws_subnet" "public-subnet-1" {
   vpc_id            = aws_vpc.wordpress-vpc.id
   cidr_block        = "10.32.100.0/24"
-  availability_zone = "eu-central-1b"
+  availability_zone = "eu-central-1a"
   map_public_ip_on_launch= "true"
   tags = {
     Name = "public-subnet-1"
@@ -36,14 +61,14 @@ resource "aws_subnet" "public-subnet-2" {
   availability_zone = "eu-central-1b"
   map_public_ip_on_launch= "true"
   tags = {
-    Name = "public-subnet-"
+    Name = "public-subnet-2"
   }
 }
 
 resource "aws_subnet" "private-subnet-1" {
   vpc_id            = aws_vpc.wordpress-vpc.id
   cidr_block        = "10.32.10.0/24"
-  availability_zone = "eu-central-1b"
+  availability_zone = "eu-central-1a"
   tags = {
     Name = "private-subnet-1"
   }
@@ -54,30 +79,47 @@ resource "aws_subnet" "private-subnet-2" {
   cidr_block        = "10.32.11.0/24"
   availability_zone = "eu-central-1b"
   tags = {
-    Name = "private-subnet-1"
+    Name = "private-subnet-2"
   }
 }
 
 
-# # 3. Create Custom Route Table
-resource "aws_route_table" "dev-route-table" {
-  vpc_id = aws_vpc.wordpress-vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw-dev.id
-  }
 
+
+# # 5.1 Create NAT gateway
+resource "aws_eip" "nat" {
+  domain = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public-subnet-1.id
   tags = {
-    Name = "Dev"
+    Name = "nat-gateway"
   }
 }
+
 
 # # 5. Associate subnet with Route Table
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.public-subnet-1.id
   route_table_id = aws_route_table.dev-route-table.id
 }
+
+resource "aws_route_table" "private-a" {
+  vpc_id = aws_vpc.wordpress-vpc.id
+  tags = {
+    Name = "private-route-table-1"
+  }
+}
+
+resource "aws_route" "private_route_a" {
+  route_table_id         = aws_route_table.private-a.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
 
 # # 6. Create Security Group to allow port 22,80,443
 resource "aws_security_group" "allow_web" {
@@ -132,7 +174,7 @@ resource "aws_network_interface" "wordpress-able-nic" {
 # resource "aws_eip" "one" {
 #   network_interface         = aws_network_interface.wordpress-able-nic.id
 #   associate_with_private_ip = "10.32.100.11"
-#   depends_on                = [aws_internet_gateway.igw-dev]
+#   depends_on                = [aws_internet_gateway.wordpress-IGW]
 # }
 
 # # 9. Create Linux2 server
